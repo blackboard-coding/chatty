0d7e93966c55122093a9d79cd4a6cf4b6d6686cf
Nutthapat Piasangka <n.devs260340@gmail.com>
2020-03-29 01:16:52 +0700

    update

--- src/lib/socket/index.js
+++ src/lib/socket/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import socketIOClient from 'socket.io-client';
 
-const url = "http://localhost:4000/";
+// const url = "http://localhost:4000/";
+const url = "https://chatty-serve.herokuapp.com/";
 
 const socket = socketIOClient(url)
 
